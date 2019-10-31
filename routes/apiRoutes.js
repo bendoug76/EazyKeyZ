117cc01ad689441b046da5d31c918e24c1319794
Benjamin Lee <benjamindlee4@gmail.com>
2019-10-31 11:50:39 -0500

    added models and api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,22 +3,58 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+    db.Ticket.findAll({}).then(function(dbTicket) {
+      res.json(dbTicket);
     });
   });
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+    db.Ticket.create(req.body).then(function(dbTicket) {
+      res.json(dbTicket);
+    });
+  });
+//time returning
+  app.post("/api/examples", function(req, res) {
+
+    req.body = {
+      ticketNum: req.body.ticketNum,
+      userName: req.body.userName,
+      modelName: req.body.modelName,
+      returnTime: req.body.returnTime,}
+
+    // newDbEntry = {
+    //   ticketnumber: 'idhtiushjdt',
+    //   name: 'dsniufnsdf'
+    // }
+
+    var pickuptime = moment().add(req.body.timetilreturn, 'm')
+    moment()
+
+
+
+    db.Example.create(newDbEntry).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+    db.Ticket.destroy({ where: { id: req.params.id } }).then(function(dbTicket) {
+      res.json(dbTicket);
     });
+  });
+
+  // PUT route for updating posts
+  app.put("/api/posts", function(req, res) {
+    db.Ticket.update(req.body,
+      {
+        where: {
+          ticketNum: req.body.ticketNum
+        }
+      })
+      .then(function(dbTicket) {
+        res.json(dbTicket);
+      });
   });
 };
