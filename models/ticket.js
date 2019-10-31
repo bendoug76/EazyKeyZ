81bdbc92846939177c1a16d390c66f283db30f4a
Benjamin Lee <benjamindlee4@gmail.com>
2019-10-31 12:16:18 -0500

    new api routes

---
+++ models/ticket.js
@@ -0,0 +1,37 @@
+module.exports = function(sequelize, DataTypes) {
+  var Example = sequelize.define("Ticket", {
+    ticketNum: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+
+    userName: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1]
+      } 
+    },
+
+    modelName: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+
+    returnTime: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+
+  });
+  return Ticket;
+};
